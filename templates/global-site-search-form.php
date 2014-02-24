8fb8543afeb6b73ff1f32c6fc1aff1775f4984d6
eugene <eugene@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-02-24 14:37:23 +0000

    Fixed global site search form action URL issue.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/global-site-search@12982 f53fe46e-8c02-459b-9fdd-93e75771b278

--- templates/global-site-search-form.php
+++ templates/global-site-search-form.php
@@ -1,5 +1,5 @@
-<?php global $current_site ?>
-<form action="<?php echo esc_url( trailingslashit( $current_site->path . global_site_search_get_search_base() ) ) ?>">
+<?php global $current_blog ?>
+<form action="<?php echo esc_url( trailingslashit( $current_blog->path . global_site_search_get_search_base() ) ) ?>">
 	<table border="0" cellpadding="2" cellspacing="2" style="width:100%">
 		<tr>
 			<td style="width:80%">
